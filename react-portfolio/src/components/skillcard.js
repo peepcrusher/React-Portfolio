e1f39ecae22d0a190817118979a48fa66a40b25e
Sam Thompson <electronic62794@yahoo.com>
2019-07-05 20:00:49 -0700

    skillcard skeleton created added materialize css, and components folder

---
+++ react-portfolio/src/components/skillcard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SkillCard(props){
+    return(
+        <div className="col s3">
+        <div className="card skillCard">
+            <div className="card-content">
+                <span className="card-title">Title</span>
+
+                <p>Javascript</p>
+            </div>
+
+        </div>
+        </div>
+    )
+}
+
+export default SkillCard;
