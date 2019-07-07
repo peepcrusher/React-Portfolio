377856556d5ec32720accf3ef83ed9c928e081b2
Sam Thompson <electronic62794@yahoo.com>
2019-07-06 20:34:20 -0700

    made a reusable component for different sections of the portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SkillCard from "./components/skillcard";
+import Section from './components/section';
 
 function App() {
   return (
     <div>
+      <Section
+      name="blue">
       <div className="row">
         <SkillCard />
       </div>
+      </Section>
     </div>
   );
 }
